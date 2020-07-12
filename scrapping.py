4a8620a584dfac14d62f998b7e73e4937698c0e7
Mitesh Rajan <miteshrajan01@gmail.com>
2020-07-12 22:15:08 +0100

    Script updated and listings are saved in csv file.

--- scrapping.py
+++ scrapping.py
@@ -1,16 +1,44 @@
+
 from utils import *
 from bs4 import BeautifulSoup
 import re, requests, os
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
+main_body_id = "_1TImB"
+price_section_id = "_2sFaY"
+gallery_id = "_3cRjW"#"_28cEs"
+URL = "https://www.bearspace.co.uk/purchase?page=10"
+block_id = "_3Xnzg"
 
-# blocks IDs fetched by inspecting the element of the webpage
-main_body_id = "_1TImB" # from URL = "https://www.bearspace.co.uk/purchase"
-price_section_id = "_2sFaY" #from individual product page
-gallery_id = "_28cEs"
+products = available_listings(URL, block_id) #find the available products (except out of stock)
+print("Number of available products: {}".format(len(products)))
 
-products = get_href(URL, block_id)
-print("{} products found in total.".format(len(products)))
+listings = []
+
+for product in products:
+    soup = soup_content(product, main_body_id)
+    title = get_title(product, block_id, main_body_id)
+    price = get_price(product, block_id, price_section_id)
+    # print(price)
+    # gallery_soup = soup_content(product, gallery_id)
+    # gallery_root = gallery_soup.find("p")
+    gallery_description = gallery_element(product, gallery_id, "p") #upto here this is dn
+    # print(gallery_description)
+    media, width, height = get_media_n_dimension(gallery_description)
+    print("URL: {}, Title: {}, Media: {}, Width: {}, Height: {}".format(product, title, media, width, height))
+    entries = {
+        'url': product,
+        'title':  title,
+        'media': media,
+        'height_cm': height,
+        'width_cm': width,
+        'price_gbp': price        
+    }
+    listings.append(entries)
+print(listings)
+
+df = pd.DataFrame.from_dict(listings)
+
+df.to_csv("py_listings.csv")
+
 
